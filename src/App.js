f23941d2544abf4289248da14a461c7c2569d04a
YaroslavLazorenko <lazorenko85@gmail.com>
2021-12-05 18:42:29 +0200

    Complete 1st step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const INITIAL_STATE = { name: '' };
+
+class App extends Component {
+  state = { contacts: [], name: '' };
+
+  resetForm = () => {
+    this.setState(INITIAL_STATE);
+  };
+
+  handleCange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, { id: nanoid(), name: this.state.name }] };
+    });
+
+    this.resetForm();
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <form action="#" onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={this.handleCange}
+          />
+
+          <button type="submit">Add contact</button>
+          <h2>Contacts</h2>
+          <ul>
+            {contacts.map(contact => {
+              return <li key={contact.name}>{contact.name}</li>;
+            })}
+          </ul>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
